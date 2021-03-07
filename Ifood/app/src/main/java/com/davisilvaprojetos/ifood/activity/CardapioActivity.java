fd259cdf0494afe076962659bc417917a61bbab1
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2021-03-07 16:32:18 -0300

    -Criando tela de Configurações do usuário -Configurando a tela de cardápio para adicionar itens do pedido

--- Ifood/app/src/main/java/com/davisilvaprojetos/ifood/activity/CardapioActivity.java
+++ Ifood/app/src/main/java/com/davisilvaprojetos/ifood/activity/CardapioActivity.java
@@ -6,15 +6,21 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.davisilvaprojetos.ifood.R;
 import com.davisilvaprojetos.ifood.adapter.AdapterProduto;
 import com.davisilvaprojetos.ifood.helper.ConfiguracaoFirebase;
+import com.davisilvaprojetos.ifood.helper.UsuarioFirebase;
 import com.davisilvaprojetos.ifood.model.Empresa;
 import com.davisilvaprojetos.ifood.model.Produto;
+import com.davisilvaprojetos.ifood.model.Usuario;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,17 +30,22 @@ import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 import java.util.List;
 
+import dmax.dialog.SpotsDialog;
+
 public class CardapioActivity extends AppCompatActivity {
 
     private RecyclerView recyclerProdutosCardapio;
     private ImageView imageEmpresaCardapio;
     private TextView textNomeEmpresaCardapio;
     private Empresa empresaSelecionada;
+    private AlertDialog dialog;
 
     private AdapterProduto adapterProduto;
     private List<Produto> produtos = new ArrayList<>();
     private DatabaseReference firebaseRef;
     private String idEmpresa;
+    private String idUsuarioLogado;
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,7 +75,42 @@ public class CardapioActivity extends AppCompatActivity {
 
         //Recuperar Produtos para a empresa
         recuperarProdutos();
+        //Recuperar os dados do usuário
+        recuperarDadosUsuario();
 
+    }
+
+    private void recuperarDadosUsuario(){
+        dialog = new SpotsDialog.Builder()
+        .setContext(this)
+        .setMessage("Carregando dados")
+        .setCancelable(false)
+        .build();
+
+        dialog.show();
+
+        DatabaseReference usuariosRef = firebaseRef
+                .child("usuarios")
+                .child(idUsuarioLogado);
+        usuariosRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.getValue() != null){
+                    usuario = snapshot.getValue(Usuario.class);
+                }
+
+                recuperarPedido();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void recuperarPedido() {
+        dialog.dismiss();
     }
 
     private void recuperarProdutos(){
@@ -89,6 +135,24 @@ public class CardapioActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_cardapio, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuPedido:
+
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private void inicializarComponentes() {
         //Configurações Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -101,6 +165,7 @@ public class CardapioActivity extends AppCompatActivity {
         textNomeEmpresaCardapio = findViewById(R.id.textNomeEmpresaCardapio);
 
         firebaseRef = ConfiguracaoFirebase.getFirebase();
+        idUsuarioLogado = UsuarioFirebase.getIdUsuario();
 
     }
 }
